26fcf6f6a1de6b796239726c6b40d3a15cb84fbf
Richard Allaway <richard.allaway@gmail.com>
2025-09-11 02:01:36 +0000

    use unqualified image name

--- features/support/container_helper.rb
+++ features/support/container_helper.rb
@@ -1,14 +1,19 @@
 module ContainerHelper
   attr_reader :test_image, :container, :base_image, :image_name
 
-  def image_name(suffix = nil)
+  def image_name(suffix = nil, local: false)
     base = ENV["MATRIX_BASE"]
     stream = ENV["MATRIX_STREAM"]
     suffix_string = suffix ? "-#{suffix}" : ""
     if base && stream
-      "ghcr.io/ublue-os/#{base}#{suffix_string}:#{stream}"
+      local_image = "#{base}#{suffix_string}:#{stream}"
     else
       raise "unknown image"
+    end
+    if local
+      local_image
+    else
+      "ghcr.io/ublue-os/#{local_image}"
     end
   end
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@
 Before do |scenario|
   attach("BASE_IMAGE=#{image_name}", "text/plain")
   if (ENV['CI'])
-    @base_image = Image.prebuilt(image_name("howdy"))
+    @base_image = Image.prebuilt(image_name("howdy", local: true))
   else 
     @base_image = Image.new("base-image")
     @base_image.build!("Containerfile")
