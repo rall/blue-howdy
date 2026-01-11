be29234bbf3974ccf6d0d958b9232bd977d08b02
Richard Allaway <richard.allaway@gmail.com>
2026-01-11 16:42:19 -0500

    Add howdy-authselect package for seamless PAM integration
    The ronnypfannschmidt/howdy-beta COPR provides howdy-authselect which
    handles PAM configuration via authselect rather than manual edits.

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,5 +3,5 @@ set -uexo pipefail
 
 dnf5 -y --refresh copr enable ronnypfannschmidt/howdy-beta
 # install packages into the ostree deployment
-rpm-ostree install howdy howdy-gtk
+rpm-ostree install howdy howdy-gtk howdy-authselect
 dnf5 -y copr disable ronnypfannschmidt/howdy-beta
