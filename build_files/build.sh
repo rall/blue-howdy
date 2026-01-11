da83ac27541d561b407a3c172e702488d34c2c2c
Richard Allaway <richard.allaway@gmail.com>
2026-01-11 15:59:23 -0500

    Switch to ronnypfannschmidt/howdy-beta COPR
    The principis/howdy-beta COPR hasn't been updated for Fedora 43 and
    fails with missing python3dist(dlib) and python3-elevate dependencies.
    
    ronnypfannschmidt/howdy-beta is a fork that includes:
    - howdy-authselect: Seamless authselect integration for Silverblue/Kinoite
    - SELinux policy fixes: Proper SELinux support out of the box
    - udev rules: Correct device permissions
    
    See: https://github.com/boltgolt/howdy/issues/1069

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -uexo pipefail
 
-dnf5 -y --refresh copr enable principis/howdy-beta
+dnf5 -y --refresh copr enable ronnypfannschmidt/howdy-beta
 # install packages into the ostree deployment
 rpm-ostree install howdy howdy-gtk
-dnf5 -y copr disable principis/howdy-beta
+dnf5 -y copr disable ronnypfannschmidt/howdy-beta
